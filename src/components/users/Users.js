50c83b075699a14c01154ef55e3a46b4c02d3ec2
Joao Carreira <joaocarreira@me.com>
2019-08-25 10:17:22 +0100

    feat: added a loading spinner as a React fragment

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
   }
-}
+  return (
+    <div style={userStyle}>
+      {users.map(user => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem'
+};
+
+Users.propTypes = {
+  users: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 export default Users;
